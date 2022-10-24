9ba40b3ac8d9a5adcc7a3790581d0926b4efa450
Joey Chen <joeychen@admins-iMac-Pro.local>
2022-10-24 11:54:13 +0800

    update script dns_reolv.py

--- dns_resolv.py
+++ dns_resolv.py
@@ -1,8 +1,6 @@
 #!/home/joey/Ansible/py3.8.10 python
 from datetime import date
-import re
 import sys
-#import dnspython as dns
 import dns.resolver
 
 today = date.today().strftime("%Y%m%d")
@@ -18,7 +16,7 @@ def getARecord(dnsServer, domainName):
     resolver.nameservers = [dnsServer]
     answers = resolver.resolve(domainName, 'A')
     result_list = []
-    with open('/home/joey/Ansible/roles/firewall_cisco/others/dns-resolved-{}-{}.txt'.format(domainName, today), 'w')  as f:
+    with open('/home/jenkins/Ansible/dns_resolving_records/dns-resolved-{}-{}.txt'.format(domainName, today), 'w')  as f:
         for answer in answers:
 #            result_list.append(str(answer))
             print('[*] {} has IP'.format(domainName), answer, 'resolved.')
